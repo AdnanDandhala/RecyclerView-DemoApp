7e0d7c7b417c28f92307d958fd10bfd4e4fa0d70
AdnanDandhala <adnan@justcodenow.com>
2022-02-03 16:28:24 +0530

    Initial commit

--- app/src/main/java/com/example/test_kotlin/CustomImage.kt
+++ app/src/main/java/com/example/test_kotlin/CustomImage.kt
@@ -3,13 +3,13 @@ package com.example.test_kotlin
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 
-@BindingAdapter("imageById")
+@BindingAdapter(value = ["imageById"])
 fun ImageView.setImageId(id: Int) {
     this.setImageResource(id)
 }
 
 @BindingAdapter(value = ["imageUrl"])
-fun setImageUrl(imageView: ImageView, id : Int) {
+fun setImageUrl(imageView: ImageView, id: Int) {
     imageView.setImageResource(id)
 }
 // Ok I Have To Pass Both Image View And Id
--- app/src/main/java/com/example/test_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/test_kotlin/MainActivity.kt
@@ -1,168 +1,45 @@
 package com.example.test_kotlin
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.test_kotlin.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var dataBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setData()
+        lifecycleScope.launch(Dispatchers.IO) {
+            delay(3000L)
+            withContext(Dispatchers.Main) {
+                repeat(1) {
+                    setData()
+                }
+                delay(3000L)
+            }
+        }
     }
 
     private fun setData() {
-        val myDataList = ArrayList<LayoutHolder>()
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_FIRST_VIEW,
-                R.drawable.businessman1,
-                "John",
-                "Deo"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_SECOND_VIEW,
-                R.drawable.bussinessman2,
-                "Jack",
-                "B"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_THIRD_VIEW,
-                R.drawable.businessman1,
-                "Sam",
-                "Set"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_FIRST_VIEW,
-                R.drawable.bussinessman2,
-                "Agro",
-                "Mas"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_SECOND_VIEW,
-                R.drawable.bussinessman2,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_THIRD_VIEW,
-                R.drawable.bussinessman2,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_FIRST_VIEW,
-                R.drawable.man,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_SECOND_VIEW,
-                R.drawable.bussinessman2,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_THIRD_VIEW,
-                R.drawable.man,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_FIRST_VIEW,
-                R.drawable.person,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_SECOND_VIEW,
-                R.drawable.person,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_THIRD_VIEW,
-                R.drawable.person,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_FIRST_VIEW,
-                R.drawable.woman,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_SECOND_VIEW,
-                R.drawable.woman,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_THIRD_VIEW,
-                R.drawable.woman,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_FIRST_VIEW,
-                R.drawable.man1,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_SECOND_VIEW,
-                R.drawable.man1,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        myDataList.add(
-            LayoutHolder(
-                LayoutHolderAdapter.THE_THIRD_VIEW,
-                R.drawable.man1,
-                "Text-1",
-                "Text-2"
-            )
-        )
-        val adapter = LayoutHolderAdapter(this, myDataList)
-        binding.mainRecyclerView.layoutManager = LinearLayoutManager(this)
-        binding.mainRecyclerView.adapter = adapter
+        val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.add().observe(this, Observer {
+            if (it != null) {
+                val adapter = LayoutHolderAdapter(applicationContext, it)
+                binding.mainRecyclerView.layoutManager = LinearLayoutManager(applicationContext)
+                binding.mainRecyclerView.adapter = adapter
+            } else {
+                Toast.makeText(this, "Some Error Occur", Toast.LENGTH_SHORT).show()
+            }
+        })
+        viewModel.setDataUsingViewModel()
     }
+
 }
