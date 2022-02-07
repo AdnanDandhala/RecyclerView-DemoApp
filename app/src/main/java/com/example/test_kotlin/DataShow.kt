92924be5f7afaebf265c61a90c804fa026edde75
AdnanDandhala <adnan@justcodenow.com>
2022-02-07 15:45:33 +0530

    Initial commit

--- app/src/main/java/com/example/test_kotlin/DataShow.kt
+++ app/src/main/java/com/example/test_kotlin/DataShow.kt
@@ -3,11 +3,14 @@ package com.example.test_kotlin
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.test_kotlin.databinding.FragmentDataShowBinding
 
 
@@ -36,6 +39,21 @@ class DataShow : Fragment() {
         "Android"
     )
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home ->
+                findNavController().navigate(R.id.action_dataShow2_to_recyclerviewData2)
+        }
+        return true
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -49,8 +67,8 @@ class DataShow : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val bundle = arguments
         val finalResult = bundle?.getString("KEY_1")
+        (activity as AppCompatActivity).supportActionBar?.title = finalResult
         Log.i("TAG", bundle.toString())
-        binding.tvDataShow.text = finalResult
         setAdapter()
         binding.ListViewDataShow.setOnItemClickListener { _, _, i, _ ->
             Toast.makeText(context, "$i", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/example/test_kotlin/RecyclerviewData.kt
+++ app/src/main/java/com/example/test_kotlin/RecyclerviewData.kt
@@ -5,20 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.test_kotlin.databinding.FragmentRecyclerviewDataBinding
 
 
 class RecyclerviewData : Fragment() {
     private lateinit var binding: FragmentRecyclerviewDataBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentRecyclerviewDataBinding.inflate(layoutInflater)
-        // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        findNavController().popBackStack(R.id.dataShow2, true)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        (activity as AppCompatActivity).supportActionBar?.title = "Constraint-Task"
+        if ((activity as AppCompatActivity).supportActionBar?.isShowing != true) {
+            (activity as AppCompatActivity).supportActionBar?.show()
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
