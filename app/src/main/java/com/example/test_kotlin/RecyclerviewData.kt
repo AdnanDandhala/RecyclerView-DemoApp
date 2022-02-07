b9277cdb8e0590cf306278eb4c49ed41bc67129e
AdnanDandhala <adnan@justcodenow.com>
2022-02-07 14:51:52 +0530

    Initial commit

--- app/src/main/java/com/example/test_kotlin/RecyclerviewData.kt
+++ app/src/main/java/com/example/test_kotlin/RecyclerviewData.kt
@@ -7,12 +7,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.test_kotlin.databinding.FragmentRecyclerviewDataBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 
 class RecyclerviewData : Fragment() {
@@ -22,12 +18,12 @@ class RecyclerviewData : Fragment() {
     ): View {
         binding = FragmentRecyclerviewDataBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
-        lifecycleScope.launch(Dispatchers.IO) {
-            withContext(Dispatchers.Main) {
-                setData()
-            }
-        }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setData()
     }
 
     private fun setData() {
