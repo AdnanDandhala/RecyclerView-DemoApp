67da264a68b5834dcf0196d7d4d1e5f324ac3fea
AdnanDandhala <adnan@justcodenow.com>
2022-02-11 18:10:02 +0530

    Initial commit

--- app/src/main/java/com/example/test_kotlin/fragments/DataShowFragment.kt
+++ app/src/main/java/com/example/test_kotlin/fragments/DataShowFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -121,6 +122,9 @@ class DataShowFragment : Fragment() {
         val tempTime = DateTimeFormatter.ofPattern("dd.MM.yyyy. HH:mm:ss")
         val answer: String = current.format(tempTime)
         val message: String
+        binding.etSendMessage.doAfterTextChanged {
+
+        }
         if (!TextUtils.isEmpty(binding.etSendMessage.text)) {
             binding.etSendMessage.defaultFocusHighlightEnabled = false
             binding.tvTime.text = answer
