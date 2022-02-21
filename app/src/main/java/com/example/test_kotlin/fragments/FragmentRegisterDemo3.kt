d76c804802136503d38b48d864d3ade7c1b29621
AdnanDandhala <adnan@justcodenow.com>
2022-02-21 19:10:32 +0530

    Initial commit

--- app/src/main/java/com/example/test_kotlin/fragments/FragmentRegisterDemo3.kt
+++ app/src/main/java/com/example/test_kotlin/fragments/FragmentRegisterDemo3.kt
@@ -4,16 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import com.example.test_kotlin.R
+import com.example.test_kotlin.databinding.FragmentRegistorDemo3Binding
+
+val spinnerItems = arrayOf(
+    "Ahmedabad",
+    "Rajkot",
+    "Mumbai",
+    "Bihar",
+    "America",
+    "London"
+)
 
 class FragmentRegisterDemo3 : Fragment() {
-
+    private lateinit var binding: FragmentRegistorDemo3Binding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registor_demo3, container, false)
+        binding = FragmentRegistorDemo3Binding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = ArrayAdapter(
+            requireContext(), android.R.layout.simple_list_item_1,
+            spinnerItems
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.cityDropDown.adapter = adapter
     }
 }
